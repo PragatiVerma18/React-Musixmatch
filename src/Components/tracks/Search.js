df1ce87661cbbb441af55f5111908f612ce3a47d
pragati <itispragativerma@gmail.com>
2020-06-06 23:52:37 +0530

    completed

---
+++ src/Components/tracks/Search.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Consumer } from '../../context';
+
+class Search extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			trackTitle: '',
+		};
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	handleSubmit = (dispatch, e) => {
+		e.preventDefault();
+		axios
+			.get(
+				`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+			)
+			.then((res) => {
+				dispatch({
+					type: 'SEARCH_TRACKS',
+					payload: res.data.message.body.track_list,
+				});
+
+				this.setState({
+					trackTitle: '',
+				});
+			})
+			.catch((err) => console.log(err));
+	};
+
+	render() {
+		return (
+			<Consumer>
+				{(value) => {
+					const { dispatch } = value;
+					return (
+						<div className='card mb-3'>
+							<div className='card-body mb-4 p-4'>
+								<h1 className='display-4 text-center'>
+									<i className='fas fa-music'></i> Search For A Song
+								</h1>
+								<p className='lead text-center'>Get the lyrics for any song</p>
+								<form onSubmit={this.handleSubmit.bind(this, dispatch)}>
+									<div className='form-group'>
+										<input
+											type='text'
+											className='form-control input-lg'
+											placeholder='Song Title...'
+											name='trackTitle'
+											value={this.state.trackTitle}
+											onChange={this.handleChange}
+										/>
+									</div>
+									<button className='btn btn-primary btn-lg btn-block mb-3'>
+										Get Track Lyrics
+									</button>
+								</form>
+							</div>
+						</div>
+					);
+				}}
+			</Consumer>
+		);
+	}
+}
+
+export default Search;
--- src/context.js
+++ src/context.js
@@ -3,6 +3,18 @@ import axios from 'axios';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'SEARCH_TRACKS':
+			return {
+				...state,
+				track_list: action.payload,
+				heading: 'Search Results',
+			};
+		default:
+			return state;
+	}
+};
 export class Provider extends Component {
 	constructor(props) {
 		super(props);
@@ -10,6 +22,7 @@ export class Provider extends Component {
 		this.state = {
 			track_list: [],
 			heading: 'Top 10 Tracks',
+			dispatch: (action) => this.setState((state) => reducer(state, action)),
 		};
 	}
 
